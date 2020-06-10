a8e94ad4533b2998ef71aa00f3df28034e79b860
kyllinn <L606782569@163.com>
2020-06-11 04:49:52 +0800

    blblm package

--- R/blblm.R
+++ R/blblm.R
@@ -1,8 +1,23 @@
+#' @importFrom utils capture.output
 #' @import purrr
 #' @import stats
+#' @import parallel
+#' @import readr
 #' @importFrom magrittr %>%
 #' @details
-#' Linear Regression with Little Bag of Bootstraps
+#' Bag of little bootstraps works by averaging the results of bootstrapping multiple subsets. BLB is well suited to modern parallel architectures.
+#'
+#' @param formula an object of class "formula" (or one that can be coerced to that class): a symbolic description of the model to be fitted.
+#' @param data data frame, list or environment (or object coercible by as.data.frame to a data frame) containing the variables in the model.
+#' @param file_list a character vector. file names that containing data.
+#' @param path character. the path of the file list.
+#' @param m the number of subsamples.
+#' @param B the repeat time of bootstrapping for each subsample.
+#' @param parallel logical. If TRUE the function will use parallelization.
+#' @param num_cl numeric. The number of clusters to be created for parallelization.
+#' @param logit logical. If TRUE the function will do logistic regression.
+#'
+#' @return an object of class "blblm"
 "_PACKAGE"
 
 
@@ -12,59 +27,96 @@ utils::globalVariables(c("."))
 
 
 #' @export
-blblm <- function(formula, data, m = 10, B = 5000) {
-  data_list <- split_data(data, m)
-  estimates <- map(
-    data_list,
-    ~ lm_each_subsample(formula = formula, data = ., n = nrow(data), B = B))
+blblm <- function(formula, data = NULL, file_list = NULL, path= '', m = 10, B = 5000, parallel = FALSE, num_cl = 0, logit = FALSE) {
+
+  # check if a file list exists
+  if (length(file_list) > 0) {
+
+    # check if parallelization is needed
+    if (parallel) {
+      cl <- makeCluster(num_cl) ## create clusters
+      estimates <- parLapply(cl,
+                             file_list,
+                             function(x) {lm_each_subsample(formula = formula, data = readr::read_csv(paste(path, x, sep = '')), logit = logit, n = nrow(readr::read_csv(paste(path, x, sep = ''))), B = B)})
+      stopCluster(cl)
+    } else {
+      estimates <- map(
+        file_list,
+        ~ lm_each_subsample(formula = formula, data = readr::read_csv(paste(path, x, sep = '')), logit = logit, n = nrow(readr::read_csv(paste(path, x, sep = ''))), B = B))
+    }
+
+  } else {
+
+    data_list <- split_data(data, m)
+
+    # check if parallelization is needed
+    if (parallel) {
+      cl <- makeCluster(num_cl)
+      estimates <- parLapply(cl,
+                             data_list,
+                             function(x) {lm_each_subsample(formula = formula, data = x, logit = logit, n = nrow(data), B = B)})
+      stopCluster(cl)
+    } else {
+      estimates <- map(
+        data_list,
+        ~ lm_each_subsample(formula = formula, data = ., logit = logit, n = nrow(data), B = B))
+    }
+  }
+
   res <- list(estimates = estimates, formula = formula)
   class(res) <- "blblm"
   invisible(res)
 }
 
 
-#' split data into m parts of approximated equal sizes
+# split data into m parts of approximated equal sizes
 split_data <- function(data, m) {
   idx <- sample.int(m, nrow(data), replace = TRUE)
   data %>% split(idx)
 }
 
 
-#' compute the estimates
-lm_each_subsample <- function(formula, data, n, B) {
-  replicate(B, lm_each_boot(formula, data, n), simplify = FALSE)
+# compute the estimates
+lm_each_subsample <- function(formula, data, logit, n, B) {
+  replicate(B, lm_each_boot(formula, data, logit, n), simplify = FALSE)
 }
 
 
-#' compute the regression estimates for a blb dataset
-lm_each_boot <- function(formula, data, n) {
+# compute the regression estimates for a blb dataset
+lm_each_boot <- function(formula, data, logit, n) {
+  # generate the weights for data
   freqs <- rmultinom(1, n, rep(1, nrow(data)))
-  lm1(formula, data, freqs)
+  lm1(formula, data, logit, freqs)
 }
 
 
-#' estimate the regression estimates based on given the number of repetitions
-lm1 <- function(formula, data, freqs) {
+# estimate the regression estimates based on given the number of repetitions
+lm1 <- function(formula, data, logit, freqs) {
   # drop the original closure of formula,
   # otherwise the formula will pick a wront variable from the global scope.
   environment(formula) <- environment()
-  fit <- lm(formula, data, weights = freqs)
+  # check if logistic regression is needed.
+  if (logit) {
+    fit <- glm(formula, data, family = binomial, weights = freqs)
+  } else {
+    fit <- lm(formula, data, weights = freqs)
+  }
   list(coef = blbcoef(fit), sigma = blbsigma(fit))
 }
 
 
-#' compute the coefficients from fit
-blbcoef <- function(fit) {
-  coef(fit)
+# compute the coefficients from fit
+blbcoef <- function(object) {
+  coef(object)
 }
 
 
-#' compute sigma from fit
-blbsigma <- function(fit) {
-  p <- fit$rank
-  y <- model.extract(fit$model, "response")
-  e <- fitted(fit) - y
-  w <- fit$weights
+# compute sigma from fit
+blbsigma <- function(object) {
+  p <- object$rank
+  y <- model.extract(object$model, "response")
+  e <- fitted(object) - y
+  w <- object$weights
   sqrt(sum(w * (e^2)) / (sum(w) - p))
 }
 
@@ -93,6 +145,7 @@ sigma.blblm <- function(object, confidence = FALSE, level = 0.95, ...) {
   }
 }
 
+
 #' @export
 #' @method coef blblm
 coef.blblm <- function(object, ...) {
@@ -105,7 +158,7 @@ coef.blblm <- function(object, ...) {
 #' @method confint blblm
 confint.blblm <- function(object, parm = NULL, level = 0.95, ...) {
   if (is.null(parm)) {
-    parm <- attr(terms(fit$formula), "term.labels")
+    parm <- attr(terms(object$formula), "term.labels")
   }
   alpha <- 1 - level
   est <- object$estimates
--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -1,3 +1,49 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("fit works", {
+
+  x <- rnorm(1000)
+  y <- rnorm(1000)
+  data <- as.data.frame(cbind(x,y))
+  names(data) <- c('x', 'y')
+
+  fit <- blblm(y~x, data, logit = FALSE, m = 3, B = 100)
+  expect_s3_class(fit, 'blblm')
+
+  co <- coef(fit)
+  expect_equal(length(co), 2)
+
+  ci <- confint(fit)
+  expect_equal(dim(ci), c(1,2))
+
+  sigma_ci <- sigma(fit, confidence = TRUE)
+  expect_equal(length(sigma_ci), 3)
+
+  pred <- predict(fit, data.frame(x = c(2.5, 3)))
+  expect_equal(length(pred), 2)
+
+  pred_ci <- predict(fit, data.frame(x = c(2.5, 3)), confidence = TRUE)
+  expect_equal(dim(pred_ci), c(2,3))
+
+
+  z <- as.integer(rnorm(1000) > 1)
+  data1 <- as.data.frame(cbind(x,z))
+  names(data1) <- c('x', 'z')
+
+  fit1 <- blblm(z~x, data1, logit = TRUE, m = 3, B = 100)
+  expect_s3_class(fit1, 'blblm')
+
+  co1 <- coef(fit1)
+  expect_equal(length(co1), 2)
+
+  ci1 <- confint(fit1)
+  expect_equal(dim(ci1), c(1,2))
+
+  sigma_ci1 <- sigma(fit1, confidence = TRUE)
+  expect_equal(length(sigma_ci1), 3)
+
+  pred1 <- predict(fit1, data.frame(x = c(2.5, 3)), type="response")
+  expect_equal(length(pred1), 2)
+
+  pred_ci1 <- predict(fit1, data.frame(x = c(2.5, 3)), , type="response", confidence = TRUE)
+  expect_equal(dim(pred_ci1), c(2,3))
+
 })
